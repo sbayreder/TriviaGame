3775e18b6b4afdf2f01fd638f909256f235d55ba
sbayreder <sbayreder@gmail.com>
2020-03-10 10:58:58 -0500

    fixed buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,31 +9,29 @@ var unanswered = 0;
 
   //Questions
   var questions=[{
-    ques: "what type of fish was Nemo?",
+    ques: "What type of fish was Nemo?",
     ans: ["goby", "clownfish", "angelfish", "cuttlefish"],
     corr: "clownfish"
   },
-  // {
-
-  // }
+  {
+    ques: "What is the fastest fish",
+    ans: ["swordfish", "mako shark", "tuna", "sailfish"],
+    corr: "sailfish"
+  },
+  {
+    ques: "What is the worlds largest fish?",
+    ans: ["whale shark", "sunfish", "lionfish", "giant squid"],
+    corr: "whale shark"
+  },
+  {
+    ques: "What is the most popular seafood in America?",
+    ans: ["crab", "cod", "shrimp", "salmon"],
+    corr: "shrimp"
+  },
 ]
-console.log(questions[0].ques);
-// var ques1 = ["What type of fish was Nemo?"];
-var ques2 = ["What is the fastest fish?"];
-var ques3 = ["What is the worlds largest fish?"];
-var ques4 = ["What is the most popular seafood in America?"];
-
-  //Answers
-// var ans1 = ;
-var ans2 = ["swordfish", "mako shark", "tuna", "sailfish"];
-var ans3 = ["whale shark", "sunfish", "lionfish", "giant squid"];
-var ans4 = ["crab", "cod", "shrimp", "salmon"];
 
 
-// var correct1 =["goby"]
-var correct2 =["sailfish"]
-var correct3 =["whale shark"]
-var correct4 =["shrimp"]
+
   //show question and answers options
   for (var i = 0; i < questions.length; i++) {
     // Write question to html
@@ -44,15 +42,6 @@ var correct4 =["shrimp"]
    
     }
 
-// $(".question2").text(ques2);
-// $(".question3").text(ques3);
-// $(".question4").text(ques4);
-
-// $(".answer1").text(ans1);
-// $(".answer2").text(ans2);
-// $(".answer3").text(ans3);
-// $(".answer4").text(ans4);
-
 
 
 //how do i make the buttons show up under the question?
@@ -61,16 +50,21 @@ for (var j = 0; j < questions[i].ans.length; j++) {
     var btn = document.createElement("button");
     var t = document.createTextNode( questions[i].ans[j]);
     btn.append(t);
-    document.body.append(btn);
+    btn.addClass("notChecked");
+    btn.attr("checked", false);
     var num = j+1;
-    $(".answer"+num).text();
+    $(".answer"+num).append(btn);
     }
 }
 
-//copy above code for all answer var
 
 
   //if correct answer is picked
+  //var htmlBtn = "<btn class="notChecked" checked=false></btn>";
+  $(document).on("click","btn",function(){
+      var currentClicked = $(this).attr("checked");
+      $(this).attr("checked",!currentClicked);
+  });
 
   //if wrong answer is picked
 
